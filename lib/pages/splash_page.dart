d6611665003749b90ad1a92f41f1dc8531297d88
irfanagust <agustiawanirfan866@gmail.com>
2022-06-25 18:32:14 +0700

    add : login interface

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flavel/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () => Navigator.pushNamed(context, '/sign-in'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(
+        height: 150,
+        width: 130,
+        decoration: BoxDecoration(
+            image:
+                DecorationImage(image: AssetImage('assets/image_splash.png'))),
+      )),
+      backgroundColor: bgColor1,
+    );
+  }
+}

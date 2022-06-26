a3338633d41fcc5266035dff5f913f9e7c4762ab
irfanagust <agustiawanirfan866@gmail.com>
2022-06-26 16:55:56 +0700

    add : categories navigation bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -6,11 +6,212 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, Irfan',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Text(
+                    '@irfantiawan',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: regular,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/image_profile.png',
+                    ),
+                  )),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget categories() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SizedBox(
+                width: defaultMargin,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: const EdgeInsets.only(
+                  right: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.circular(
+                    12,
+                  ),
+                ),
+                child: Text(
+                  'All Shoes',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 13,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: const EdgeInsets.only(
+                  right: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: transparentColor,
+                  border: Border.all(color: subtitleTextColor),
+                  borderRadius: BorderRadius.circular(
+                    12,
+                  ),
+                ),
+                child: Text(
+                  'Casual',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 13,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: const EdgeInsets.only(
+                  right: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: transparentColor,
+                  border: Border.all(color: subtitleTextColor),
+                  borderRadius: BorderRadius.circular(
+                    12,
+                  ),
+                ),
+                child: Text(
+                  'Badminton',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 13,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: const EdgeInsets.only(
+                  right: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: transparentColor,
+                  border: Border.all(color: subtitleTextColor),
+                  borderRadius: BorderRadius.circular(
+                    12,
+                  ),
+                ),
+                child: Text(
+                  'Running',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 13,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: const EdgeInsets.only(
+                  right: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: transparentColor,
+                  border: Border.all(color: subtitleTextColor),
+                  borderRadius: BorderRadius.circular(
+                    12,
+                  ),
+                ),
+                child: Text(
+                  'Hikking',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 13,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: const EdgeInsets.only(
+                  right: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: transparentColor,
+                  border: Border.all(color: subtitleTextColor),
+                  borderRadius: BorderRadius.circular(
+                    12,
+                  ),
+                ),
+                child: Text(
+                  'Basketball',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 13,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+        categories(),
+      ],
     );
   }
 }

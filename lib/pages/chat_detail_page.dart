7c091527665296bf0baa9b9573d8c9961b9e913b
irfanagust <agustiawanirfan866@gmail.com>
2022-06-28 10:20:11 +0700

    add : chat preview product interface

--- lib/pages/chat_detail_page.dart
+++ lib/pages/chat_detail_page.dart
@@ -1,47 +1,117 @@
+import 'dart:ui';
+
 import 'package:flavel/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class ChatDetailPage extends StatelessWidget {
   const ChatDetailPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Widget chatInput() {
+    Widget productPreview() {
       return Container(
-        margin: const EdgeInsets.all(20),
+        width: 225,
+        height: 74,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(bottom: 20),
+        decoration: BoxDecoration(
+          color: bgColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: primaryColor),
+        ),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                height: 45,
-                decoration: BoxDecoration(
-                  color: bgColor4,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                child: Center(
-                  child: TextFormField(
-                    style: primaryTextStyle,
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'Type Message...',
-                      hintStyle: subtitleTextStyle,
-                    ),
-                  ),
-                ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes3.png',
+                fit: BoxFit.cover,
               ),
             ),
             const SizedBox(
-              width: 20,
+              width: 10,
             ),
-            GestureDetector(
-              onTap: () {},
-              child: Image.asset(
-                'assets/button_send.png',
-                width: 45,
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hi-Qua Badminton V2 2.0',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: regular,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium,
+                      fontSize: 14,
+                    ),
+                  )
+                ],
               ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 22,
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: bgColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Image.asset(
+                    'assets/button_send.png',
+                    width: 45,
+                  ),
+                ),
+              ],
             ),
           ],
         ),

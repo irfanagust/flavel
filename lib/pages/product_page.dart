de42b4e471d76c23e4c345f1c21092bc591e2d1c
irfanagust <agustiawanirfan866@gmail.com>
2022-07-20 16:22:20 +0700

    add : interfave header detail product

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -6,12 +6,40 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: 20,
+              left: defaultMargin,
+              right: defaultMargin,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Icon(
+                    Icons.arrow_back_ios_new,
+                  ),
+                ),
+                Icon(
+                  Icons.shopping_bag,
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text(
-          'product page',
-          style: primaryTextStyle,
-        ),
+      backgroundColor: bgColor6,
+      body: ListView(
+        children: [
+          header(),
+        ],
       ),
     );
   }
